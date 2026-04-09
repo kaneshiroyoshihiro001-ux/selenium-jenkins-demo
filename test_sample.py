44c510180be915226420b716a39d61388d7210a5
kaneshiroyoshihiro001-ux <kaneshiroyoshihiro001@gmail.com>
2026-04-09 11:53:29 +0900

    Update test_sample.py

--- test_sample.py
+++ test_sample.py
@@ -1,11 +1,10 @@
-cat > /var/jenkins_home/test_sample.py <<'PY'
 import pytest
 from python_org_page import PythonOrgPage
 
 @pytest.mark.parametrize("keyword, should_have_results", [
     ("pycon", True),
     ("selenium", True),
-    ("zzzz_no_result_keyword_12345", False),
+    ("zzzz_no_result_keyword_12345", True),
 ])
 def test_python_search(driver, keyword, should_have_results):
     page = PythonOrgPage(driver)
@@ -19,4 +18,3 @@ def test_python_search(driver, keyword, should_have_results):
         assert not page.has_no_results()
     else:
         assert page.has_no_results()
-PY
