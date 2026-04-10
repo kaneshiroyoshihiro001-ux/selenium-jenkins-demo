41248d08ab488ceb62e0664cd0f8bee3a00b1ed4
kaneshiroyoshihiro001-ux <kaneshiroyoshihiro001@gmail.com>
2026-04-10 16:01:55 +0900

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,19 +1,19 @@
-import pytest
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.remote.webdriver import WebDriver
+# import pytest
+# from selenium import webdriver
+# from selenium.webdriver.chrome.options import Options
+# from selenium.webdriver.remote.webdriver import WebDriver
 
 
-@pytest.fixture
-def driver() -> WebDriver:
-    options = Options()
-    options.add_argument("--headless=new")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
+# @pytest.fixture
+# def driver() -> WebDriver:
+#     options = Options()
+#     options.add_argument("--headless=new")
+#     options.add_argument("--no-sandbox")
+#     options.add_argument("--disable-dev-shm-usage")
 
-    driver = webdriver.Remote(
-        command_executor="http://chrome:4444/wd/hub",
-        options=options,
-    )
-    yield driver
-    driver.quit()
+#     driver = webdriver.Remote(
+#         command_executor="http://chrome:4444/wd/hub",
+#         options=options,
+#     )
+#     yield driver
+#     driver.quit()
